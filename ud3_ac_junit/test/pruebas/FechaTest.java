4149c9876fc2eb19a8cf50bca378c8b9ab551bd5
Profesor <Profesor@DEP09.informatica.comercio>
2024-02-19 20:17:16 +0100

    Crear FechaTest

--- ud3_ac_junit/test/pruebas/FechaTest.java
+++ ud3_ac_junit/test/pruebas/FechaTest.java
@@ -12,6 +12,40 @@ import static org.junit.Assert.*;
  * @author Profesor
  */
 public class FechaTest {
-    
+
+Fecha fecha = new Fecha();
+
+	@Test
+	public void CP1_FormatoFecha1() {
+		String orden = "\\d{4}/\\d{2}";
+		int n = 1;
+		String resultado = fecha.devuelveFecha(n);
+		assertTrue(resultado.matches(orden));
+
+	}
+	@Test
+	public void CP2_FormatoFecha2() {
+		String orden = "\\d{2}/\\d{4}";
+		int n = 2;
+		String resultado = fecha.devuelveFecha(n);
+		assertTrue(resultado.matches(orden));
+
+	}
+	@Test
+	public void CP3_FormatoFecha3() {
+		String orden = "\\d{2}/\\d{2}";
+		int n = 3;
+		String resultado = fecha.devuelveFecha(n);
+		assertTrue(resultado.matches(orden));
+
+	}
+	@Test
+	public void CP4_FormatoFechaError() {
+		String orden = "ERROR";
+		int n = 4;
+		String resultado = fecha.devuelveFecha(n);
+		assertTrue(resultado.matches(orden));
+
+	}    
         
 }
